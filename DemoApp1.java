ebacd88e79bc2019367e128a6f9c37a689515687
Amolbari <86976717+Amolbari@users.noreply.github.com>
2021-07-11 15:54:11 +0530

    CLS:DemoApplications

---
+++ DemoApp1.java
@@ -0,0 +1,10 @@
+class DemoApp1 
+{
+	public void DemoApp(){
+	System.out.println("DemoApp2");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ DemoApp2.java
@@ -0,0 +1,10 @@
+class DemoApp2 
+{
+	public void DemoApp(){
+	System.out.println("DemoApp1");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
